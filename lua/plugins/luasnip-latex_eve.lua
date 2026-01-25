98bdb5daef943e4ac6f3e7e94ac826fe47f90807
CraftxTD <craftxtd@gmail.com>
2026-01-24 23:28:34 -0500

    fourth commit

--- lua/plugins/luasnip-latex_eve.lua
+++ lua/plugins/luasnip-latex_eve.lua
@@ -1,7 +1,10 @@
 return {
   {
     "evesdropper/luasnip-latex-snippets.nvim",
-    dependencies = { "L3MON4D3/LuaSnip" },
+    dependencies = {
+      "L3MON4D3/LuaSnip",
+      "nvim-treesitter/nvim-treesitter",
+    },
     ft = { "tex", "plaintex", "latex" }, -- only load in LaTeX files
     config = function()
       require("luasnip-latex-snippets")
